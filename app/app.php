9cf89f363e3111ffb135d312696a42ab1cabdb81
Phillip Shannon & Tyler Deem <student@epicodus.com>
2015-08-05 13:54:14 -0700

    Create delete all tasks feature

--- app/app.php
+++ app/app.php
@@ -28,13 +28,11 @@
     });
 
 
-    $app->post("/delete_tasks", function() {
+    $app->post("/delete_tasks", function() use ($app) {
         Task::deleteAll();
 
-        return "
-            <h1>List cleared!</h1>
-            <p><a href='/'>Home</a></p>
-        ";
+        return $app['twig']->render('delete_tasks.html.twig');
+
     });
 
 
